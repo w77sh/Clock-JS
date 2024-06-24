15895bdcf346a072cc166677da6ffeb6648551f3
w77sh <iraqxgmar915@gmail.com>
2024-06-25 01:47:46 +0300

    add hover effects

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -2,7 +2,8 @@ const hours = document.getElementById("hours")
 const minutes = document.getElementById("minutes")
 const seconds = document.getElementById("seconds")
 
-setInterval(function() {
+setInterval(
+    function() {
 
     const date = new Date()
     
@@ -11,4 +12,6 @@ minutes.textContent = date.getMinutes()
 seconds.textContent = date.getSeconds()
 
 
-})
+}
+
+)
